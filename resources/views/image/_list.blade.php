8dd05a094cb64149f47c5e318466bd29a33248b7
Udibagas <udibagas@gmail.com>
2016-05-30 22:54:07 +0700

    benerin salwa image

--- resources/views/image/_list.blade.php
+++ resources/views/image/_list.blade.php
@@ -1,9 +1,8 @@
 <div class="col-md-4">
 	<div class="thumbnail" style="height:270px;">
 		<a href="/image/{{ $image->id_salwaimages }}-{{ str_slug($image->judul) }}">
-			<div class="thumbnail-block">
-				<img src="/{{ $image->img_images }}" alt="{{ $image->judul }}">
-			</div>
+			<img src="/{{ $image->img_images }}" alt="{{ $image->judul }}">
+			<div class="thumbnail-block"></div>
 		</a>
 	</div>
 </div>
