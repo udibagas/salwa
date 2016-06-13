47f475d664683ff0ab04a14a94f5423dac926058
Udibagas <udibagas@gmail.com>
2016-06-14 01:04:04 +0700

    tampilan mobile kajian dan produk, alhamdulillah beres semua

--- resources/views/kajian/mobile/show.blade.php
+++ resources/views/kajian/mobile/show.blade.php
@@ -5,52 +5,50 @@
 @section('content')
 
 	<div class="row-post">
-		<div class="col-md-9">
-			<h3>{{ $kajian->kajian_tema }}</h3>
+		<h3>{{ $kajian->kajian_tema }}</h3>
 
-			<div class="">
-				<p>
-					Pemateri:<br />
-					<strong>{{ $kajian->ustadz ? $kajian->ustadz->ustadz_name : ''}}</strong>
-				</p>
-				<p>
-					Jenis Kajian:<br />
-					<strong>{{ $kajian->jenis_kajian == '1' ? 'Tematik' : 'Rutin' }}</strong>
-				</p>
+		<div class="">
+			<p>
+				Pemateri:<br />
+				<strong>{{ $kajian->ustadz ? $kajian->ustadz->ustadz_name : ''}}</strong>
+			</p>
+			<p>
+				Jenis Kajian:<br />
+				<strong>{{ $kajian->jenis_kajian == '1' ? 'Tematik' : 'Rutin' }}</strong>
+			</p>
 
-				<p>
-					Waktu:<br />
-					<strong>
-						@if ($kajian->jenis_kajian == 1)
-						{{ $kajian->kajian_dates }}
-						@elseif ($kajian->jenis_kajian == 2)
-						{{ \App\Kajian::getHari($kajian->setiap_hari) }}, {{ $kajian->setiap_jam }}
-						@else
-						{{ $kajian->setiap_tanggal }}
-						@endif
-					</strong>
-				</p>
-				<p>
-					Tempat:<br />
-					<strong>
-						{{ $kajian->kajian_tempat }}<br>
-						{{ $kajian->area ? $kajian->area->nama_area : '' }} - {{ $kajian->lokasi ? $kajian->lokasi->nama_lokasi : '' }}
-					</strong>
-				</p>
+			<p>
+				Waktu:<br />
+				<strong>
+					@if ($kajian->jenis_kajian == 1)
+					{{ $kajian->kajian_dates }}
+					@elseif ($kajian->jenis_kajian == 2)
+					{{ \App\Kajian::getHari($kajian->setiap_hari) }}, {{ $kajian->setiap_jam }}
+					@else
+					{{ $kajian->setiap_tanggal }}
+					@endif
+				</strong>
+			</p>
+			<p>
+				Tempat:<br />
+				<strong>
+					{{ $kajian->kajian_tempat }}<br>
+					{{ $kajian->area ? $kajian->area->nama_area : '' }} - {{ $kajian->lokasi ? $kajian->lokasi->nama_lokasi : '' }}
+				</strong>
+			</p>
 
-				<p>
-					Contact Person:<br />
-					<strong>
-						@if ($kajian->pic1)
-						{{ $kajian->pic1->pic_name }} - {{ $kajian->pic1->pic_phone }} <br />
-						@endif
+			<p>
+				Contact Person:<br />
+				<strong>
+					@if ($kajian->pic1)
+					{{ $kajian->pic1->pic_name }} - {{ $kajian->pic1->pic_phone }} <br />
+					@endif
 
-						@if ($kajian->pic2)
-						{{ $kajian->pic2->pic_name }} - {{ $kajian->pic2->pic_phone }} <br />
-						@endif
-					</strong>
-				</p>
-			</div>
+					@if ($kajian->pic2)
+					{{ $kajian->pic2->pic_name }} - {{ $kajian->pic2->pic_phone }} <br />
+					@endif
+				</strong>
+			</p>
 		</div>
 	</div>
 
