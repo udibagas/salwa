b7eb3ae57ab8e174080112d4be2f83124b5bf098
Udibagas <udibagas@gmail.com>
2016-06-27 13:12:54 +0700

    pertanyaan

--- resources/views/pertanyaan/mobile/show.blade.php
+++ resources/views/pertanyaan/mobile/show.blade.php
@@ -3,27 +3,32 @@
 @section('title', 'Tanya Ustadz : {{ $pertanyaan->judul_pertanyaan }}')
 
 @section('content')
-
 <div class="row-post">
-	<h3>Pertanyaan: {{ $pertanyaan->judul_pertanyaan }}</h3>
-	<small>
-		<strong>
-			@if ($pertanyaan->user)
-			{{ $pertanyaan->user->jenis_kelamin == 'p' ? 'Ikhwan' : 'Akhwat' }}
-			@endif
-
-			@if ($pertanyaan->daerah_asal) ({{ $pertanyaan->daerah_asal }}) @endif
-		</strong>
-
-		<span class="">
-			asked {{ $pertanyaan->tgl_tanya ? $pertanyaan->tgl_tanya->diffForHumans() : "" }}
-			@if ($pertanyaan->group)
-			on <a href="/pertanyaan/?group_id={{ $pertanyaan->group_id }}">{{ $pertanyaan->group->group_name }}</a>
-			@endif
-		</span>
-
-	</small>
-	<br>
+	<h3>{{ $pertanyaan->judul_pertanyaan}}</h3>
+</div>
+<div class="row-post text-center">
+	@include('layouts._share')
+</div>
+<div class="row-post">
+	<div class="media">
+		<div class="media-left">
+			<div class="thumbnail" style="height:40px;width:40px;">
+				<img class="media-object profile" alt="{{ $pertanyaan->judul_pertanyaan }}" data-name="{{ $pertanyaan->judul_pertanyaan }}">
+			</div>
+		</div>
+		<div class="media-body">
+			<strong>
+				{{ $pertanyaan->user ? $pertanyaan->user->jenis_kelamin == 'p' ? 'Ikhwan' : 'Akhwat' : '' }}
+				@if ($pertanyaan->daerah_asal) ({{ $pertanyaan->daerah_asal }}) @endif
+			</strong>
+			<div class="text-muted">
+				{{ $pertanyaan->tgl_tanya ? $pertanyaan->tgl_tanya->diffForHumans() : "" }}
+				@if ($pertanyaan->group)
+				on <a href="/pertanyaan/?group_id={{ $pertanyaan->group_id }}">{{ $pertanyaan->group->group_name }}</a>
+				@endif
+			</div>
+		</div>
+	</div>
 	<br>
 
 	{!! $pertanyaan->ket_pertanyaan !!}
@@ -32,23 +37,36 @@
 
 <div class="row-post">
 	@if ($pertanyaan->jawaban)
-		<h3>Jawaban:</h3>
+		<div class="media">
+			<div class="media-left">
+				<div style="width:40px;height:40px;">
+					@if ($pertanyaan->ustadz && $pertanyaan->ustadz->img_user)
+					<img class="media-object img-circle cover" src="/{{ $pertanyaan->ustadz->img_user }}" />
+					@else
+					<img class="media-object img-circle cover" src="/images/logo-padding.png" style="border: 1px solid #D9EDF7;" />
+					@endif
+				</div>
+			</div>
+			<div class="media-body">
 
-		<small>
-			<strong>{{ $pertanyaan->ustadz ? $pertanyaan->ustadz->name : '' }}</strong>
-			<span class="text-muted">answered {{ $pertanyaan->tgl_jawab ? $pertanyaan->tgl_jawab->diffForHumans() : "" }}</span>
-		</small>
+				@if (auth()->check() && auth()->user()->user_id == $pertanyaan->dijawab_oleh)
+				<div class="pull-right">
+					<a href="/pertanyaan/{{$pertanyaan->pertanyaan_id}}/jawab" class="btn btn-xs btn-info"><i class="fa fa-edit"></i> Edit Jawaban</a>
+				</div>
+				@endif
 
-		<br><br>
+				<strong>{{ $pertanyaan->ustadz ? $pertanyaan->ustadz->name : '' }}</strong>
+				<div class="text-muted">
+					{{ $pertanyaan->tgl_jawab ? $pertanyaan->tgl_jawab->diffForHumans() : "" }}
+				</div>
 
+			</div>
+		</div>
+		<br>
 		{!! $pertanyaan->jawaban !!}
 	@else
 	<strong class="text-danger">Belum ada jawaban untuk pertanyaan terkait.</strong>
 	@endif
-</div>
-
-<div class="row-post text-center">
-	@include('layouts._share')
 </div>
 
 <h3 class="title">Pertanyaan Terkait</h3>
