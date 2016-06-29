4a9a8c2457165ac98dbb5a66486ea6b1dc01f41c
Udibagas <udibagas@gmail.com>
2016-06-29 13:06:10 +0700

    swipe diilangin dulu

--- resources/views/layouts/navMobile.blade.php
+++ resources/views/layouts/navMobile.blade.php
@@ -1,15 +1,17 @@
-<div class="container-fluid mobile-nav">
-	<div class="row">
-		<div class="col-xs-4" style="padding:8px 15px;font-size:20px;">
-			<a id="menu" href="#sidr-main" style="color:#fff;"><i class="fa fa-bars"></i></a>
-		</div>
-		<div class="col-xs-4 text-center">
-			<img src="/images/logo-shadow.png" alt="" style="height:50px;" />
-		</div>
-		<div class="col-xs-4 text-right" style="padding:8px 15px;font-size:20px;">
-			@if (url()->current() != route('home') && url()->current() != route('home1'))
-			<a id="menu-right" href="#sidr-right" style="color:#fff;"><i class="fa fa-search"></i></a>
-			@endif
+<div class="mobile-nav">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-4" style="padding:8px 15px;font-size:20px;">
+				<a id="menu" href="#sidr-main" style="color:#fff;"><i class="fa fa-bars"></i></a>
+			</div>
+			<div class="col-xs-4 text-center">
+				<img src="/images/logo-shadow.png" alt="" style="height:50px;" />
+			</div>
+			<div class="col-xs-4 text-right" style="padding:8px 15px;font-size:20px;">
+				@if (url()->current() != route('home') && url()->current() != route('home1'))
+				<a id="menu-right" href="#sidr-right" style="color:#fff;"><i class="fa fa-search"></i></a>
+				@endif
+			</div>
 		</div>
 	</div>
 </div>
