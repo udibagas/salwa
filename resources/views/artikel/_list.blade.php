e68e2c676967225d4de5786b328ab747d8b4e054
Udibagas <udibagas@gmail.com>
2016-07-01 16:41:13 +0700

    dwdwqd

--- resources/views/artikel/_list.blade.php
+++ resources/views/artikel/_list.blade.php
@@ -3,11 +3,11 @@
 		<a href="/artikel/{{ $artikel->artikel_id }}-{{ str_slug($artikel->judul) }}">
 			<img src="/{{ $artikel->img_artikel }}" alt="{{ $artikel->judul }}">
 			<div class="thumbnail-block">
-				<div class="caption">
-					<h4>{{ $artikel->judul }}</h4>
-					{{ $artikel->user ? $artikel->user->name : '' }}<br />
-					<em>{{ $artikel->updated->diffForHumans() }}</em>
-				</div>
+			</div>
+			<div class="caption">
+				<h4>{{ $artikel->judul }}</h4>
+				{{ $artikel->user ? $artikel->user->name : '' }}<br />
+				<em>{{ $artikel->updated->diffForHumans() }}</em>
 			</div>
 		</a>
 	</div>
