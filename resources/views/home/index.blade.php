4574184de3e826ee8e25a68d3d089cac08623a22
Udibagas <udibagas@gmail.com>
2016-07-01 16:39:46 +0700

    banner lebaran

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -44,6 +44,23 @@
 		</div>
 	</div>
 
+	<?php if (strtotime(date('Y-m-d')) <= strtotime('2016-07-11')) : ?>
+	<div class="gal-item">
+		<div class="box">
+			<div class="modal fade" id="popup" tabindex="-1" role="dialog">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+						<div class="modal-body">
+							<img src="/images/lebaran.jpg" class="img-responsive" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endif; ?>
+
 @stop
 
 @push('script')
