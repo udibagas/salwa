389d73dcedbd24121f7b8be253bf57518453aa6b
Udibagas <udibagas@gmail.com>
2016-07-03 14:24:40 +0700

    add edit & hapus button on post

--- resources/views/pertanyaan/mobile/_form-jawab.blade.php
+++ resources/views/pertanyaan/mobile/_form-jawab.blade.php
@@ -13,7 +13,7 @@
 	</div>
 
 	<div class="form-group{{ $errors->has('ket_pertanyaan') ? ' has-error' : '' }}">
-		{{ Form::textarea('ket_pertanyaan', $pertanyaan->ket_pertanyaan, ['rows' => 10, 'class' => 'form-control', 'placeholder' => 'Keterangan Pertanyaan']) }}
+		{{ Form::textarea('ket_pertanyaan', strip_tags($pertanyaan->ket_pertanyaan), ['rows' => 10, 'class' => 'form-control', 'placeholder' => 'Keterangan Pertanyaan']) }}
 
 		@if ($errors->has('ket_pertanyaan'))
 		<span class="help-block">
