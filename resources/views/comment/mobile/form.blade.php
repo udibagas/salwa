5d2efdde09484a42dce2729c0386996e523423b5
Udibagas <udibagas@gmail.com>
2016-07-03 11:58:27 +0700

    tambah tombol add di semua bagian untuk user admin

--- resources/views/comment/mobile/form.blade.php
+++ resources/views/comment/mobile/form.blade.php
@@ -1,48 +1,34 @@
+<h4 class="title">Tulis Komentar</h4>
 <div class="row-post">
-	<div class="media">
-		<div class="media-left media-middle">
-			<div class="thumbnail" style="height:40px;width:40px;">
-				<img class="media-object cover" @if (auth()->user()->img_user) src="/{{auth()->user()->img_user}}" @else src="/images/nobody.png" @endif">
-			</div>
+	{!! Form::model($comment, ['url' => $url, 'method' => $method]) !!}
+
+		{!! Form::hidden('commentable_id', $comment->commentable_id) !!}
+		{!! Form::hidden('commentable_type', $comment->commentable_type) !!}
+		{!! Form::hidden('redirect', url()->current()) !!}
+
+		<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+			{!! Form::text('title', $comment->title, ['class' => 'form-control', 'placeholder' => 'Judul Komentar']) !!}
+
+			@if ($errors->has('title'))
+			<span class="help-block">
+				<strong>{{ $errors->first('title') }}</strong>
+			</span>
+			@endif
 		</div>
-		<div class="media-body" style="padding-top:10px;">
-			<strong>
-				{{ $header or 'Tulis' }} Komentar
-			</strong>
+
+		<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
+			{!! Form::textarea('content', $comment->content, ['class' => 'form-control', 'rows' => 4, 'placeholder' => 'Komentar Anda']) !!}
+
+			@if ($errors->has('content'))
+			<span class="help-block">
+				<strong>{{ $errors->first('content') }}</strong>
+			</span>
+			@endif
 		</div>
-		<hr style="margin-top:5px;">
 
-		{!! Form::model($comment, ['url' => $url, 'method' => $method, 'class' => 'comment']) !!}
+		<div class="form-group">
+			<button type="submit" name="submit" class="btn btn-info"><i class="fa fa-send"></i> Kirim Komentar</button>
+		</div>
 
-			{!! Form::hidden('commentable_id', $comment->commentable_id) !!}
-			{!! Form::hidden('commentable_type', $comment->commentable_type) !!}
-			{!! Form::hidden('redirect', url()->current()) !!}
-
-			<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-				{!! Form::text('title', $comment->title, ['class' => 'form-control', 'placeholder' => 'Judul Komentar']) !!}
-
-				@if ($errors->has('title'))
-				<span class="help-block">
-					<strong>{{ $errors->first('title') }}</strong>
-				</span>
-				@endif
-			</div>
-
-			<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
-				{!! Form::textarea('content', $comment->content, ['class' => 'form-control', 'rows' => 4, 'placeholder' => 'Komentar Anda']) !!}
-
-				@if ($errors->has('content'))
-				<span class="help-block">
-					<strong>{{ $errors->first('content') }}</strong>
-				</span>
-				@endif
-			</div>
-
-			<div class="form-group">
-				<button type="submit" name="submit" class="btn btn-info"><i class="fa fa-send"></i> Kirim Komentar</button>
-			</div>
-
-		{!! Form::close() !!}
-
-	</div>
+	{!! Form::close() !!}
 </div>
--- resources/views/layouts/edit-btn-mobile.blade.php
+++ resources/views/layouts/edit-btn-mobile.blade.php
@@ -1,1 +1,3 @@
-<img class="profile img-circle" data-name="EDIT" style="position:fixed;bottom:90px;right:20px;" data-font-size="12" data-char-count="4" />
+<a href="{{ url()->current() }}/edit">
+	<img class="profile img-circle" data-name="EDIT" style="position:fixed;bottom:90px;right:20px;" data-font-size="12" data-char-count="4" />
+</a>
