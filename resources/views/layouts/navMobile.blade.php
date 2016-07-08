ed76da7365a0b94c5360889ed6abc4d63d6d12dd
Udibagas <udibagas@gmail.com>
2016-07-09 00:25:06 +0700

    benahi menubar

--- resources/views/layouts/navMobile.blade.php
+++ resources/views/layouts/navMobile.blade.php
@@ -3,8 +3,8 @@
 		<div class="row">
 			<div class="col-xs-8" style="padding-right:0;margin-right:0;">
 				<a id="menu" href="#sidr-main"><i class="fa fa-bars"></i></a>
-				<a href="/" class="logo text-bold text-white text-right" style="font-size:17px;">
-					<img src="/images/logo-shadow.png" alt="" style="height:35px;margin-left:8px;display:inline-block;margin-top:-11px;" /> SalamDakwah
+				<a href="/" class="logo text-white text-right" style="font-size:17px;">
+					<img src="/images/logo-shadow.png" alt="" style="height:35px;margin-left:8px;display:inline-block;margin-top:-9px;" /> SalamDakwah
 				</a>
 			</div>
 			<div class="col-xs-4 text-right" style="padding-left:0;margin-left:0;">
