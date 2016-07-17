885ebafa29fdbd386022ecf59c4e5bf0ab91c520
Udibagas <udibagas@gmail.com>
2016-07-17 21:59:28 +0700

    update timeline

--- resources/views/timeline/mobile/_item.blade.php
+++ resources/views/timeline/mobile/_item.blade.php
@@ -56,7 +56,7 @@
 		<br>
 		{!! nl2br(str_replace('&nbsp;',' ',strip_tags(explode('|||', $p->content)[1]))) !!}
 	@else
-		{!! strip_tags($p->content, '<b><i><strong><img><br><br /><ul><li><ol>') !!}
+		{!! nl2br(strip_tags($p->content, '<b><i><strong><img><br><br /><ul><li><ol>')) !!}
 	@endif
 	</div>
 
