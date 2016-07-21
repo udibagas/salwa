131410698fdef0374d2063fe32a049587ad1b3c2
Udibagas <udibagas@gmail.com>
2016-07-22 01:53:21 +0700

    commit semua bro..

--- resources/views/quran/_share.blade.php
+++ resources/views/quran/_share.blade.php
@@ -3,7 +3,11 @@
 	<a href="https://www.facebook.com/sharer.php?u=http://www.salamdakwah.com/quran/{{ $ayah }}" class="btn btn-default" target="_blank"><i class="fa fa-facebook"></i></a>
 	<a href="https://twitter.com/share?url=http://www.salamdakwah.com/quran/{{ $ayah }}" class="btn btn-default"><i class="fa fa-twitter" target="_blank"></i></a>
 	<a href="https://plus.google.com/share?url=http://www.salamdakwah.com/quran/{{ $ayah }}" class="btn btn-default"><i class="fa fa-google-plus" target="_blank"></i></a>
-	<!-- <a href="#" class="btn btn-default"><i class="fa fa-clone"></i></a> -->
+</div>
+
+<div class="btn-group">
+	<a href="#" class="btn btn-default"><i class="fa fa-clone"></i></a>
+	<a href="#" class="btn btn-default"><i class="fa fa-download"></i></a>
 </div>
 
 <a href="#" class="btn btn-default play-ayat"><i class="fa fa-play"></i></a>
