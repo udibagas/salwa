7212f77bfd74f9361c5feb7c79558330357fb0a0
Bagas Udi Sahsangka <udibagas@gmail.com>
2016-08-08 01:51:52 +0700

    handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,7 +35,7 @@ class Handler extends ExceptionHandler
     {
         if (\App::environment('production')) {
             if ($e instanceof \Exception) {
-                \Mail::send('emails.exception', ['code' => $e->getCode(), 'message' => $e->getMessage(), 'trace' => $e->getTraceAsString()], function ($m) {
+                \Mail::send('emails.exception', ['code' => $e->getCode(), 'message' => $e->getMessage(), 'trace' => $e->getTraceAsString()], function ($m) use ($e) {
                     $m->to('udibagas@gmail.com', 'Bagas Udi Sahsangka')->subject('SalamDakwah | ERROR '.$e->getCode().': '.$e->getMessage());
                 });
             }
