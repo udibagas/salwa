e152f0468d63eca8279a2de62ac549c5fbcca93a
Bagas Udi Sahsangka <udibagas@gmail.com>
2016-09-15 20:07:41 +0700

    ilangin popup

--- resources/views/home/_popup.blade.php
+++ resources/views/home/_popup.blade.php
@@ -1,4 +1,4 @@
-<?php if (strtotime(date('Y-m-d')) <= strtotime('2016-10-14')) : ?>
+<?php if (strtotime(date('Y-m-d')) <= strtotime('2016-09-14')) : ?>
 <div class="gal-item">
 	<div class="box">
 		<div class="modal fade" id="popup" tabindex="-1" role="dialog">
